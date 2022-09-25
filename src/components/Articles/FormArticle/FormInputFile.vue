fb097ece3283e837f7aebf5e3e13a3f43df381ab
TontonRitonBzh <henri.dc@hotmail.fr>
2022-09-25 16:55:25 +0200

    modified:   src/components/Articles/FormArticle/FormInputFile.vue

--- src/components/Articles/FormArticle/FormInputFile.vue
+++ src/components/Articles/FormArticle/FormInputFile.vue
@@ -1,55 +1,50 @@
 <template>
-    <div id="container-input-picture">
-        <label id="label-picture-article" for="picture-article">
-            <i class="fa-regular fa-image"></i>
-            Ajouter une photo
-            <p>{{ fileName }}</p>
-            <input :onchange="fillName" type="file" name="picture-article" id="picture-article" ref="input"/>
-        </label>
+  <div>
+    <div
+      class="imagePreviewWrapper"
+      :style="{ 'background-image': `url(${previewImage})` }"
+      @click="selectImage">
     </div>
+    <input
+      ref="fileInput"
+      type="file"
+      @input="pickFile">
+  </div>
 </template>
-
 <script>
-
 export default {
-    props:{
-        currentFile:String
+  data() {
+      return {
+        previewImage: null
+      };
     },
-    data(){
-        return{
-            fileName:'',
+  methods: {
+      selectImage () {
+          this.$refs.fileInput.click()
+      },
+      pickFile () {
+        let input = this.$refs.fileInput
+        let file = input.files
+        if (file && file[0]) {
+          let reader = new FileReader
+          reader.onload = e => {
+            this.previewImage = e.target.result
+          }
+          reader.readAsDataURL(file[0])
+          
         }
-    },
-    methods:{
-        fillName(){
-            console.log('fillname')
-            this.fileName=this.$refs.input.files[0].name
-        }
-    },
-    updated(){
-            console.log('update')
-            if(this.currentFile!==''){this.fileName=this.currentFile}
-    }
-    
+      }
+  }
 }
 </script>
-
-<style scoped>
-    #label-picture-article {
-  cursor: pointer;
-}
-
-#picture-article {
-  display: none;
-}
-
-#container-input-picture{
-  border:1px solid black;
-  width: 85%;
-  margin: auto;
-}
-
-p{
-    color:red;
+<style scoped >
+.imagePreviewWrapper {
+    width: 250px;
+    height: 250px;
+    display: block;
+    cursor: pointer;
+    margin: 0 auto 30px;
+    background-size: cover;
+    background-position: center center;
 }
 </style>
