478919833b6c9694050e70b7d2b150cce147a554
TontonRitonBZH <106626989+TontonRitonBZH@users.noreply.github.com>
2022-10-31 18:20:47 +0100

    commit on master

--- api/database.config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    return [
-        'host'     => 'db.3wa.io',
-        'dbname'   => 'henridecargouet_projet',
-        'user'     => 'henridecargouet',
-        'password' => 'ca9e88646dd136996bfac1501ca085db',
-    ];
