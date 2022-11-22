5781da7bc50e57ec5737c7cff0c985e8d36523a0
TontonRitonBZH <106626989+TontonRitonBZH@users.noreply.github.com>
2022-11-22 11:18:01 +0100

    ter

--- src/main.js
+++ src/main.js
@@ -8,7 +8,6 @@ import { createPinia } from "pinia";
 import { welcomeModale } from "./composables/theWelcomeFunctions";
 
 let cookiesTab = document.cookie;
-console.log(cookiesTab);
 if (cookiesTab.indexOf("mode") === -1) {
   welcomeModale();
   setTimeout(createMyApplication, 6000);
@@ -21,8 +20,9 @@ function createMyApplication() {
   const app = createApp(App).use(createPinia());
   app.use(router);
   app.mount("#app");
-  console.log("main");
   let theWelcome = document.querySelector("#welcomeModale");
-  let body = document.querySelector("body");
-  body.removeChild(theWelcome);
+  if (theWelcome) {
+    let body = document.querySelector("body");
+    body.removeChild(theWelcome);
+  }
 }
